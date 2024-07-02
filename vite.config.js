380f4e608630fc17b06c2e4e3f19f471085a00e3
puffywonton <wontonking123@outlook.com>
2024-07-02 17:54:58 +0200

    new file path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/soniq-demo/"
+  base: "/soniq-demo/",
+  build: {
+    assetsDir: 'assets'
+  }
 })
